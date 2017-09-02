cff8a3b2f39378223a59707465b07efb595c47db
Leo <gavinlj1992@163.com>
2017-09-03 00:41:50 +0800

    添加了测试用例

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 import unittest, time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊丽丝听说有一个很酷的在线代办事项应用
         # 她去看了个这个应用的首页
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # 她注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
@@ -37,10 +38,12 @@ class NewVisitorTest(unittest.TestCase):
         # 伊丽丝的爱好是使用假蝇做饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
 
-        # 她按回车键后，页面更新了
+        # 她按回车键后，被带到了一个新URL
         # 待办事项表格中显示了“1: Buy peacok feathers”
         inputbox.send_keys((Keys.ENTER))
         time.sleep(1)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
@@ -55,14 +58,38 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # 伊丽丝想知道这个网站是否会记住她的清单
+        # 现在一个叫作弗朗西斯的新用户访问了网站
 
-        # 她看到网站为她生成了一个唯一的URL
-        # 而且页面中又一些文字解说这个功能
-        self.fail('Finish the test!')
-        # 她访问那个URL，发现她的待办事项列表还在
+        ## 我们使用一个新浏览器会话
+        ## 确保伊丽丝的信息不会从cookie中泄漏出来
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
 
-        # 她很满意，去睡觉了
+        # 弗朗西斯访问首页
+        # 页面中看不到伊丽丝的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+        # 弗朗西斯输入一个新待办事项，新建一个清单
+        # 他不像伊丽丝那样兴趣盎然
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # 弗朗西斯获得了他的唯一URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 这个页面还是没有伊丽丝的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 两人都很满意，去睡觉了
+
+
+
